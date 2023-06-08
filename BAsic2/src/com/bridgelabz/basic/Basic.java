d3a969d61d15971362d50cd49790647bd6db6e67
MohitaKhurana <sachdevamohita5@gmail.com>
2023-06-08 16:27:15 +0530

    third commit

--- BAsic2/src/com/bridgelabz/basic/Basic.java
+++ BAsic2/src/com/bridgelabz/basic/Basic.java
@@ -2,7 +2,7 @@ package com.bridgelabz.basic;
 
 public class Basic {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println(" i am using Intellij");
+
+        System.out.println(" smile please");
     }
 }
